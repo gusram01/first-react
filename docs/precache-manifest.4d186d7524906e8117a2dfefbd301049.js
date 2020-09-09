c751e70a35aebed11ad31664f6ab906565cd8fbb
Gus Ramirez <g.ram.bt@hotmail.com>
2020-09-09 17:42:52 -0500

    add docs folder

---
+++ docs/precache-manifest.4d186d7524906e8117a2dfefbd301049.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a051f37469bc5da6f7ec9e9d4d7ec63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1d42351f3ad8f9c42b2",
+    "url": "/static/css/main.1609e40e.chunk.css"
+  },
+  {
+    "revision": "5f26341cf7af13537b77",
+    "url": "/static/js/2.f8730f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8730f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1d42351f3ad8f9c42b2",
+    "url": "/static/js/main.f93cc19f.chunk.js"
+  },
+  {
+    "revision": "e13af855f2385358fc05",
+    "url": "/static/js/runtime-main.17c06fbb.js"
+  }
+]);
