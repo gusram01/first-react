5350a02ebc46883b2082fee81556f4da4b8e9d46
Gus Ramirez <g.ram.bt@hotmail.com>
2020-09-14 11:17:11 -0500

    Updates

---
+++ precache-manifest.d3706a0cfac4d744657ec328ee5b4365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05fcc72662f3462ceda08e6bc4d46f74",
+    "url": "/first-react/index.html"
+  },
+  {
+    "revision": "1c873bdc41fa27fc4364",
+    "url": "/first-react/static/css/main.1609e40e.chunk.css"
+  },
+  {
+    "revision": "5f26341cf7af13537b77",
+    "url": "/first-react/static/js/2.f8730f5d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react/static/js/2.f8730f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c873bdc41fa27fc4364",
+    "url": "/first-react/static/js/main.3a2bcbe1.chunk.js"
+  },
+  {
+    "revision": "ee12c2a25cb420a4fb98",
+    "url": "/first-react/static/js/runtime-main.1418545b.js"
+  }
+]);
